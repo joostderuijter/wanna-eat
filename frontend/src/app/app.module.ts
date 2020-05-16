e93490eb59a73228ca8bdc251381fca50fd7283e
joostderuijter <joostderuijter@protonmail.com>
2020-05-16 20:10:47 +0200

    added frontend form + styling

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderFormComponent } from './order-form/order-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    OrderFormComponent
   ],
   imports: [
     BrowserModule
